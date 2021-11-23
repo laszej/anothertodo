1e31db0e790268e4e6be2c55258d786631bdd8b1
laszej <88169478+laszej@users.noreply.github.com>
2021-11-23 16:01:22 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,19 +2,23 @@ const lista = document.getElementById ("lista");
 const dodaj = document.getElementById ("dodaj");
 const usuń = document.getElementById ("usuń");
 let input = document.getElementById ("input");
-
-
+ 
+ 
 dodaj.addEventListener('click', ()=>{
-let dodaneZadanie = document.createElement("div");
-dodaneZadanie.innerHTML = `<button id="usuwacz"> usuń zadanie </button> ${input.value}<br> `;
-lista.appendChild(dodaneZadanie);
-input.value = "";
-dodaneZadanie.addEventListener("click", ()=>{lista.removeChild(dodaneZadanie)})})
-
+	let dodaneZadanie = document.createElement("div");
+	let button = document.createElement("button");
+    button.classList.add("delete-btn")
+    button.innerHTML="Usuń zadanie"
+	let div = document.createElement("div");
+	div.innerHTML = input.value   
+	lista.appendChild(dodaneZadanie);
+    dodaneZadanie.appendChild(div)
+    div.appendChild(button)
+	input.value = "";
+	button.addEventListener("click", ()=>{lista.removeChild(dodaneZadanie)})
+})
+ 
 usuń.addEventListener("click", ()=>{lista.innerHTML=""})
-
-
-
 
 
 
